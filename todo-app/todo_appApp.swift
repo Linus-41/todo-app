6e24f26dedf91f559ed703f05d32f06faec64dda
Linus Widing <l.widing@web.de>
2024-09-05 22:02:48 +0200

    Initial Commit

---
+++ todo-app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo-app
+//
+//  Created by Linus Widing on 05.09.24.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
