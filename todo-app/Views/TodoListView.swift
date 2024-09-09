514170efb1bc1aa089d729995f1667aadda015aa
Linus Widing <l.widing@web.de>
2024-09-09 13:00:41 +0200

    Update TodoListView.swift

--- todo-app/Views/TodoListView.swift
+++ todo-app/Views/TodoListView.swift
@@ -9,60 +9,57 @@ struct TodoListView: View {
     }
     
     var body: some View {
-        NavigationView {
-            Group {
-                if viewModel.isLoading {
-                    ProgressView("Loading Todos...")
-                } else if let errorMessage = viewModel.errorMessage {
-                    Text("Error: \(errorMessage)")
-                        .foregroundColor(.red)
-                        .multilineTextAlignment(.center)
-                } else {
-                    List {
-                        ForEach(viewModel.todos) { todo in
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text(todo.title)
-                                        .font(.headline)
-                                    if let text = todo.text {
-                                        Text(text)
-                                            .font(.subheadline)
-                                            .foregroundColor(.gray)
-                                    }
-                                }
-                                Spacer()
-                                Image(systemName:
-                                        todo.isDone
-                                      ? "checkmark.circle.fill"
-                                      : "circle")
-                                .foregroundColor(todo.isDone
-                                                 ? .green
-                                                 : .gray)
-                                .onTapGesture {
-                                    viewModel.toggleTodoCompletion(todo)
+        NavigationStack {
+            if viewModel.isLoading {
+                ProgressView("Loading Todos...")
+            } else if let errorMessage = viewModel.errorMessage {
+                Text("Error: \(errorMessage)")
+                    .foregroundColor(.red)
+                    .multilineTextAlignment(.center)
+            } else {
+                List {
+                    ForEach(viewModel.todos) { todo in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(todo.title)
+                                    .font(.headline)
+                                if let text = todo.text {
+                                    Text(text)
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
                                 }
                             }
+                            Spacer()
+                            Image(systemName:
+                                    todo.isDone
+                                  ? "checkmark.circle.fill"
+                                  : "circle")
+                            .foregroundColor(todo.isDone
+                                             ? .green
+                                             : .gray)
+                            .onTapGesture {
+                                viewModel.toggleTodoCompletion(todo)
+                            }
                         }
-                        .onDelete(perform: deleteTodo)
                     }
+                    .onDelete(perform: deleteTodo)
                 }
-            }
-            .navigationBarTitle("To-Do List")
-            .navigationBarItems(trailing: Button(action: {
-                showingAddTodoView = true
-            }) {
-                Image(systemName: "plus")
-            })
-            .onAppear {
-                viewModel.fetchTodos()
-            }
-            .sheet(isPresented: $showingAddTodoView) {
-                AddTodoView(viewModel: AddTodoViewModel(),todoViewModel: viewModel)
+                .navigationBarTitle("ToDo")
+                .navigationBarItems(trailing: Button(action: {
+                    showingAddTodoView = true
+                }) {
+                    Image(systemName: "plus")
+                })
+                .onAppear {
+                    viewModel.fetchTodos()
+                }
+                .sheet(isPresented: $showingAddTodoView) {
+                    AddTodoView(viewModel: AddTodoViewModel(),todoViewModel: viewModel)
+                }
             }
         }
     }
     
-    // Function to handle the deletion of a todo
     private func deleteTodo(at offsets: IndexSet) {
         let indexes = offsets.map { $0 }
         
